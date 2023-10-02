61fa24c726f3dd25fd638c4ed9fd3bcbb483738b
amannitp <amannitp272@gmail.com>
2023-10-02 11:00:38 +0530

    password generator project completed

--- 05_basics/password-Generator/src/App.jsx
+++ 05_basics/password-Generator/src/App.jsx
@@ -1,25 +1,48 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback ,useEffect,useRef} from "react";
 
 function App() {
   const [passwordLength, setPasswordLength] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
-  const [password, setPassword] = useState();
+  const [password, setPassword] = useState("");
+  
+  // useref hooks
+   const passwordRef=useRef(null)
+    
+
+   const copyPasswordToClipboard=useCallback(()=>{
+    passwordRef.current?.select()
+    passwordRef.current?.setSelectionRange(0,5)
+        window.navigator.clipboard.writeText(passwordLength)
+
+       },[passwordLength])
+   
 
   const passwordGenerator = useCallback(() => {
     let pass = "";
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-    if (numberAllowed) str += "0123456789";
-    if (charAllowed) str += "!@#$%^&*()}{[]";
+    if (numberAllowed) {
+      let num='1234567890'
+      pass+=num.charAt(Math.floor(Math.random()*num.length+1));
+    
+
+    } 
+    if (charAllowed){
+      let char='!@#$%^&*()}{[]'
+      pass+=char.charAt(Math.floor(Math.random()*char.length+1));
+    } 
 
     for (let i = 1; i <= passwordLength; i++) {
-      let char = Math.floor(Math.random * str.length + 1);
+      let char = Math.floor(Math.random()* str.length + 1);
       pass += str.charAt(char);
     }
     setPassword(pass);
   }, [passwordLength, numberAllowed, charAllowed, setPassword]);
-
+  
+  useEffect(()=>{
+    passwordGenerator()
+  },[passwordLength,numberAllowed,charAllowed,passwordGenerator])
   return (
     <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-700">
       <h1 className="text-center text-white text-lg mb-3"> Password</h1>
@@ -30,8 +53,11 @@ function App() {
           placeholder="Password"
           className="outline-none w-full py-2 px-3"
           readOnly
+          ref={passwordRef}
         />
-        <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
+        <button 
+        onClick={copyPasswordToClipboard}
+        className="outline-none bg-blue-700 text-white px-3 py-0.5 ">
           copy
         </button>
       </div>
