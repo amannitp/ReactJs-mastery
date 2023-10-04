964b1ee074ceca32a678e322878a5f563697785d
amannitp <amannitp272@gmail.com>
2023-10-04 12:45:27 +0530

    React-router setup completed

---
+++ 06reactRouter/reactRouter/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+       <h1 className='bg-orange-700 text-black text-5xl p-4 text-center'>react router</h1>
+    </>
+  )
+}
+
+export default App
